5279fc470850c6a555f076a3c9ad7e430430de66
Chananel Perlmutter <hprlmwtr@gmail.com>
2022-11-08 13:05:03 +0200

    first commit of chananel

--- memory_game_chananel/index.js
+++ memory_game_chananel/index.js
@@ -1,53 +1,32 @@
 const numOfcards = 6;
+
 let cards = [];
 for (let i = 0; i < numOfcards; i++) {
   cards.push(i + 1);
   cards.push(i + 1);
 }
+
+
+const firemanSam = [
+  {0: "https://mms.businesswire.com/media/20140602005356/en/418196/5/HERO_FS_CG_Hero%5B1%5D.jpg",},
+  {1: "https://static.wikia.nocookie.net/firemansam/images/6/61/S10.Penny.M.jpeg/revision/latest?cb=20161013211808",},
+  {2: "https://static.miraheze.org/loathsomecharacterswiki/8/81/ElvisS6.png",},
+  {3: "https://static.wikia.nocookie.net/firemansam/images/a/ac/S10.Off.St.jpeg/revision/latest/scale-to-width-down/250?cb=20161014030836",},
+  {4: "https://static.wikia.nocookie.net/firemansam/images/2/28/Image-1476318271.jpg/revision/latest?cb=20161013002431",},
+  {5: "https://static.wikia.nocookie.net/firemansam/images/2/21/Ellie_Phillips_Promo.png/revision/latest?cb=20150925014912",}
+]
+
+
+
+
+
 let score = 0;
 let flippedCards = [];
-
-function shuffle(cards){
 cards = cards.sort((a, b) => Math.random() - 0.5);
 cards = cards.sort((a, b) => Math.random() - 0.5);
 cards = cards.sort((a, b) => Math.random() - 0.5);
 cards = cards.sort((a, b) => Math.random() - 0.5);
-cards = cards.sort((a, b) => Math.random() - 0.5);}
-
-
-//----------------------------------------------------------------
-
-// making a button that shuffle the cards
-
-// let input = document.getElementById("myInput");
-// input.addEventListener("keypress", function(event) {
-//   if (event.key === "Enter") {
-//     event.preventDefault();
-//     document.getElementById("myBtn").click();
-//   }
-// });
-// //----------------------------------------------------------
-
-
-
-
-//function myFunction() {
-//  document.getElementById("MIX"){
-//    cards = cards.sort((a, b) => Math.random() - 0.5);
-//  };
-//}
-
-
-// that will be a picture on every card
-
-// counting points of every 
-
-// a button of new game
-
-// memory of the results
-
-
-
+cards = cards.sort((a, b) => Math.random() - 0.5);
 
 function flipCard(event) {
   console.log("clicked");
@@ -103,3 +82,32 @@ cards.forEach((card, index) => {
 });
 
 console.log({ cards });
+
+function newGame(){
+
+  const button = document.createElement('button')
+button.innerText = 'Click for a new Game'
+button.addEventListener('click', () => {
+
+
+  let flippedCards = [];
+cards = cards.sort((a, b) => Math.random() - 0.5);
+cards = cards.sort((a, b) => Math.random() - 0.5);
+cards = cards.sort((a, b) => Math.random() - 0.5);
+cards = cards.sort((a, b) => Math.random() - 0.5);
+cards = cards.sort((a, b) => Math.random() - 0.5);
+
+
+alert('Oh, you clicked me!')
+})
+
+document.body.appendChild(button)}
+newGame();
+
+
+// button for new game
+// function newGame (){
+
+  
+
+// }
